236377e9ae2481c6f13344763acd7749fbe31480
moise <moise@Thinkpad_Moises>
2025-09-15 10:32:57 -0600

    Primer Commit: Clase Alumno.java con calculo de promedio y calificacion

--- src/evora/Alumno.java
+++ src/evora/Alumno.java
@@ -1,60 +1,44 @@
 package evora;
+import java.util.Scanner;
 
 public class Alumno {
-    //ATRIBUTOS
-    private final String Nombre; //NOMBRE DEL PARTICIPANTE
-    private final int[] Calificaciones; //ARRAY CON 5 CALIFICACIONES
-    
-    //CONSTRUCTOR
-    public Alumno(String Nombre, int[] Calificaciones){
-        this.Nombre= Nombre;
-        this.Calificaciones= Calificaciones;
-    }
-    
-    //METODO A: CALCULAR PROMEDIO.
-    public double CalcularPromedio(){
-        int Suma=0;
-        for(int Calificacion: Calificaciones){
-            Suma+= Calificacion;
-        }
-        return (double) Suma/Calificaciones.length;
-    }
-    
-    //METODO B: OBTENER CALIFICACION FINAL, SEGUN LA TABLA
-    public char ObtenerCalificacion(double Average){
-        if(Average>=91 && Average<=100){
-            return 'A';
-        } else if(Average>=81){
-            return 'B';
-        } else if(Average>=71){
-            return 'C';
-        } else if(Average>=61){
-            return 'D';
-        } else if(Average>=51){
-            return 'E';
-        } else{
-            return 'F';
-        }
-    }
-    
-    //METODO C: IMPRIMIR RESULTADOS
-    public void ImprimirResultados(double Average, char Calificacion){
-        System.out.println("NOMBRE DEL ESTUDIANTE: "+ Nombre);
-        for(int i=0; i<Calificaciones.length; i++){
-            System.out.println("CALIFICACION "+ (i+1)+ ": "+ Calificaciones[i]);
-        }
-        System.out.println("PROMEDIO: "+ Average);
-        System.out.println("CALIFICACION: "+ Calificacion);
-    }
-    
-    //METODO MAIN PARA PROBAR
     public static void main(String[] args){
-        int Notas[]= {85, 92, 78, 88, 90}; //EJEMPLO
-        Alumno Alumno1= new Alumno("Moises", Notas);
-        
-        double Average= Alumno1.CalcularPromedio();
-        char CalificacionFinal= Alumno1.ObtenerCalificacion(Average);
-        
-        Alumno1.ImprimirResultados(Average, CalificacionFinal);
+        //PEDIR NOMBRE
+        try (Scanner SC = new Scanner(System.in)) {
+            //PEDIR NOMBRE
+            System.out.println("INGRESA EL NOMBRE DEL ESTUDIANTE: ");
+            String Nombre= SC.nextLine();
+            
+            //CREAR ARRAY DE CALIFICACIONES
+            int[] Calificaciones= new int[5];
+            for(int i=0; i<5; i++){
+                System.out.println("INGRESA CALIFICACION " +(i+1)+ ": ");
+                Calificaciones[i]= SC.nextInt();
+            }
+            
+            //CALIFICACION PROMEDIO
+            double Suma=0;
+            for(int Cal: Calificaciones){
+                Suma+= Cal;
+            }
+            double Average= Suma/Calificaciones.length;
+            
+            //OBTENER CALIFICACION FINAL
+            char CalFinal;
+            if(Average>=91) CalFinal='A';
+            else if(Average>=81) CalFinal='B';
+            else if(Average>=71) CalFinal='C';
+            else if(Average>=61) CalFinal='D';
+            else if(Average>=51) CalFinal='E';
+            else CalFinal='F';
+            
+            //IMPRIMIR CALIFICACIONES
+            System.out.println("\nNOMBRE DEL ESTUDIANTE: "+ Nombre);
+            for(int i=0; i<Calificaciones.length; i++){
+                System.out.println("CALIFICACION "+ (i+1)+ ": "+ Calificaciones[i]);
+            }
+            System.out.println("PROMEDIO: "+ Average);
+            System.out.println("CALIFICACION: "+ CalFinal);
+        }
     }
 }
